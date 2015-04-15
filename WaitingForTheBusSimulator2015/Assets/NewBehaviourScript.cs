6151d94f61e19c8c66dbea822f1e1f94f0f5c2ae
crmd <cdorm@hotmail.com>
2015-04-15 18:28:43 +1000

    something soomething year of our lord video game

---
+++ WaitingForTheBusSimulator2015/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public RenderTexture rt;
+	public Camera cameraInst;
+	public GameObject phone;
+//	Material mat = new Material;
+	void Start() {
+		rt = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+		rt.Create();
+		cameraInst = /*transform.parent.*/GetComponent<Camera> ();
+		cameraInst.targetTexture = rt;
+//		mat.SetTexture ("rt", rt);
+		phone.renderer.material.mainTexture = rt;
+	}
+}
