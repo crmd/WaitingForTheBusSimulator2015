ae499c299f98c5df3a5f4cbc049b3f515265b8bc
Cameron Dorman <cdorm@hotmail.com>
2015-04-17 15:27:06 +1000

    NPCs out the wazoo, HMMMM VIDEO GAME!
    prototypes yo

--- WaitingForTheBusSimulator2015/Assets/BirdFlap.cs
+++ WaitingForTheBusSimulator2015/Assets/BirdFlap.cs
@@ -12,6 +12,12 @@ public class BirdFlap : MonoBehaviour {
 	public float pipeSpeed = 2.0f;
 	public int numPipes = 10;
 
+	private Transform player;
+
+	public bool started = false;
+
+	public int score;
+
 	// Use this for initialization
 	void Start () {
 		brid = GameObject.Find ("Brid");
@@ -24,32 +30,69 @@ public class BirdFlap : MonoBehaviour {
 			                                                 board.transform.position.z), Quaternion.identity) as GameObject;
 			activePipes.Add(pipe);
 		}
+		player = transform.parent;
+		brid.rigidbody.useGravity = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Mouse0))
+		if(started)
 		{
-			brid.rigidbody.velocity = Vector3.up * flapMultiplier;
-		}
-		for(int i = 0; i < activePipes.Count; ++i)
-		{
-			activePipes[i].transform.position = new Vector3(activePipes[i].transform.position.x - pipeSpeed * Time.deltaTime,
-			                                                activePipes[i].transform.position.y,
-			                                                activePipes[i].transform.position.z);
-			if(activePipes[i].transform.position.x < board.transform.position.x - 20.0f)
+			if(Input.GetKeyDown(KeyCode.Mouse0))
 			{
-				GameObject oldPipe = activePipes[i];
-				activePipes[i] = Instantiate(pipes, new Vector3(board.transform.position.x + 10.0f * (activePipes.Count - 2),
-				                                                board.transform.position.y + Random.Range(-2.0f, 2.0f),
-				                                                board.transform.position.z), Quaternion.identity) as GameObject;
-				GameObject.Destroy(oldPipe);
+				brid.rigidbody.velocity = Vector3.up * flapMultiplier;
 			}
+			for(int i = 0; i < activePipes.Count; ++i)
+			{
+				activePipes[i].transform.position = new Vector3(activePipes[i].transform.position.x - pipeSpeed * Time.deltaTime,
+				                                                activePipes[i].transform.position.y,
+				                                                activePipes[i].transform.position.z);
+				if(activePipes[i].transform.position.x < board.transform.position.x - 20.0f)
+				{
+					GameObject oldPipe = activePipes[i];
+					activePipes[i] = Instantiate(pipes, new Vector3(board.transform.position.x + 10.0f * (activePipes.Count - 2),
+					                                                board.transform.position.y + Random.Range(-2.0f, 2.0f),
+					                                                board.transform.position.z), Quaternion.identity) as GameObject;
+					GameObject.Destroy(oldPipe);
+				}
+			}
+		}
+		else
+		{
+			if(Input.GetKeyDown(KeyCode.Mouse0))
+			{
+				brid.rigidbody.useGravity = true;
+				brid.rigidbody.velocity = Vector3.up * flapMultiplier;
+				started = true;
+			}
+		}
+
+		if(player.gameObject.name == "OVRPlayerController")
+		{
+			Vector3 tempVec;
+			//Set Position
+			tempVec = new Vector3(0.0f, -0.287f, 0.198f);
+			transform.position = (new Vector3(tempVec.x,
+			                                  tempVec.y * player.forward.y,
+			                                  tempVec.z));
+			//Set Rotation
+			tempVec = new Vector3(330.13f, 180.0f, 0.0f);
+			transform.localRotation = Quaternion.Euler(new Vector3(tempVec.x * player.forward.x,
+			                                                       tempVec.y * player.forward.y,
+			                                                       tempVec.z * player.forward.z));
 		}
 	}
 
 	void FixedUpdate()
 	{
-		brid.rigidbody.AddForce (-Vector3.up * 5);
+		if(started)
+		{
+			brid.rigidbody.AddForce (-Vector3.up * 5);
+		}
+	}
+
+	public void AddScore()
+	{
+		score++;
 	}
 }
---
+++ WaitingForTheBusSimulator2015/Assets/BirdFlapPipe.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdFlapPipe : MonoBehaviour {
+
+	private BirdFlap flapBrid;
+
+	// Use this for initialization
+	void Start () {
+		GameObject brid = GameObject.Find ("OVRPlayerController");
+
+		if(brid == null)
+		{
+			brid = GameObject.Find ("First Person Controller");
+		}
+		flapBrid = brid.GetComponentInChildren<BirdFlap> ();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.name == "Brid")
+		{
+			flapBrid.AddScore();
+		}
+	}
+}
