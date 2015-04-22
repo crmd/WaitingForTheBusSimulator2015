6529be9d881e6abe53c01edc7ae8aaaceb0a125e
Cameron Dorman <cdorm@hotmail.com>
2015-04-22 23:27:04 +1000

    flappy video game compleet

--- WaitingForTheBusSimulator2015/Assets/BirdFlap.cs
+++ WaitingForTheBusSimulator2015/Assets/BirdFlap.cs
@@ -1,22 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+
+public enum BirdFlapState
+{
+	BFS_Ready,
+	BFS_Running,
+	BFS_Dead
+}
 
 public class BirdFlap : MonoBehaviour {
 
 	GameObject brid;
 	GameObject board;
 	public float flapMultiplier = 1000.0f;
+	[SerializeField]
 	List<GameObject> activePipes = new List<GameObject>();
 	public GameObject pipes;
 	public float pipeSpeed = 2.0f;
-	public int numPipes = 10;
+	private int numPipes = 10;
 
 	private Transform player;
 
-	public bool started = false;
+//	public bool started = false;
 
 	public int score;
+	public int highScore;
+
+	private Vector3 bridStartPos;
+	
+	private Text titleText;
+	private Text scoreText;
+	private Text gameOverText;
+	private Text gameOverScoreText;
+	private Text highScoreText;
+	private Text clickToFlapText;
+
+	public AudioSource flappyAudio;
+	public AudioClip flappyFlap;
+	public AudioClip flappyDeath;
+
+	public BirdFlapState birdFlapState = BirdFlapState.BFS_Ready;
+	
+
 
 	// Use this for initialization
 	void Start () {
@@ -32,15 +59,39 @@ public class BirdFlap : MonoBehaviour {
 		}
 		player = transform.parent;
 		brid.rigidbody.useGravity = false;
+		brid.GetComponent<BirdFlapBrid>().birdFlap = this;
+
+		bridStartPos = brid.transform.position;
+
+		//Find the flapBrid text objects
+		titleText = GameObject.Find ("TitleText").GetComponent<Text>();
+		scoreText = GameObject.Find ("ScoreText").GetComponent<Text>();
+		gameOverText = GameObject.Find ("GameOverText").GetComponent<Text>();
+		gameOverScoreText = GameObject.Find ("GameOverScoreText").GetComponent<Text>();
+		highScoreText = GameObject.Find ("GameOverHighScoreText").GetComponent<Text>();
+		clickToFlapText = GameObject.Find ("ClickToFlapText").GetComponent<Text>();
+
+		flappyAudio.clip = flappyFlap;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(started)
+		UpdateText();
+		switch(birdFlapState)
 		{
+		case BirdFlapState.BFS_Ready:
 			if(Input.GetKeyDown(KeyCode.Mouse0))
 			{
-				brid.rigidbody.velocity = Vector3.up * flapMultiplier;
+				Flap ();
+			}
+			break;
+		case BirdFlapState.BFS_Running:
+
+
+
+			if(Input.GetKeyDown(KeyCode.Mouse0))
+			{
+				Flap ();
 			}
 			for(int i = 0; i < activePipes.Count; ++i)
 			{
@@ -56,17 +107,24 @@ public class BirdFlap : MonoBehaviour {
 					GameObject.Destroy(oldPipe);
 				}
 			}
-		}
-		else
-		{
+			break;
+		case BirdFlapState.BFS_Dead:
+
+
+
 			if(Input.GetKeyDown(KeyCode.Mouse0))
 			{
-				brid.rigidbody.useGravity = true;
-				brid.rigidbody.velocity = Vector3.up * flapMultiplier;
-				started = true;
+				for(int i = 0; i < activePipes.Count; ++i)
+				{
+					GameObject.Destroy(activePipes[i]);
+				}
+				activePipes.Clear();
+				SpawnPipes();
+				brid.transform.position = bridStartPos;
+				Flap ();
 			}
+			break;
 		}
-
 		if(player.gameObject.name == "OVRPlayerController")
 		{
 			Vector3 tempVec;
@@ -83,17 +141,83 @@ public class BirdFlap : MonoBehaviour {
 		}
 	}
 
+	void SpawnPipes()
+	{
+		for (int i = 0; i < numPipes; ++i)
+		{
+			GameObject pipe = Instantiate(pipes, new Vector3(board.transform.position.x + 10.0f * (i+1),
+			                                                 board.transform.position.y + Random.Range(-2.0f, 2.0f),
+			                                                 board.transform.position.z), Quaternion.identity) as GameObject;
+			activePipes.Add(pipe);
+		}
+	}
+
+	void UpdateText()
+	{
+		switch(birdFlapState)
+		{
+		case BirdFlapState.BFS_Ready:
+			titleText.enabled = true;
+			scoreText.enabled = false;
+			gameOverText.enabled = false;
+			gameOverScoreText.enabled = false;
+			highScoreText.enabled = false;
+			clickToFlapText.enabled = true;
+			break;
+		case BirdFlapState.BFS_Running:
+			titleText.enabled = false;
+			scoreText.enabled = true;
+			gameOverText.enabled = false;
+			gameOverScoreText.enabled = false;
+			highScoreText.enabled = false;
+			clickToFlapText.enabled = false;
+			scoreText.text = "Score: " + score.ToString();
+			break;
+		case BirdFlapState.BFS_Dead:
+			titleText.enabled = false;
+			scoreText.enabled = false;
+			gameOverText.enabled = true;
+			gameOverScoreText.enabled = true;
+			highScoreText.enabled = true;
+			clickToFlapText.enabled = true;
+			highScoreText.text = "High Score: " + highScore.ToString();
+			gameOverScoreText.text = "Score: " + score.ToString();
+			break;
+		}
+	}
+
 	void FixedUpdate()
 	{
-		if(started)
+		if(birdFlapState == BirdFlapState.BFS_Running)
 		{
 			brid.rigidbody.AddForce (-Vector3.up * 5);
 		}
 	}
 
+	private void Flap()
+	{
+		brid.collider.enabled = true;
+		brid.rigidbody.useGravity = true;
+		birdFlapState = BirdFlapState.BFS_Running;
+		brid.rigidbody.velocity = Vector3.up * flapMultiplier;
+		flappyAudio.clip = flappyFlap;
+		flappyAudio.Play ();
+	}
+
+	public void BirdDeath()
+	{
+		birdFlapState = BirdFlapState.BFS_Dead;
+		if(highScore < score)
+		{
+			highScore = score;
+		}
+		brid.collider.enabled = false;
+		flappyAudio.clip = flappyDeath;
+		flappyAudio.Play();
+	}
+
 	public void AddScore()
 	{
-		Debug.Log ("AddingScore");
 		score++;
 	}
 }
---
+++ WaitingForTheBusSimulator2015/Assets/BirdFlapBrid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdFlapBrid : MonoBehaviour {
+
+	public BirdFlap birdFlap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "pipe")
+		{
+			birdFlap.SendMessage("BirdDeath", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
